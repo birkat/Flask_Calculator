6e7b809c3461a5ea72484d09a4fddd07b4386b31
birkat <sudiprauniyar@gmail.com>
2023-09-01 17:09:07 +0545

    with html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for, redirect
 
 obj = Flask(__name__)
 
@@ -6,21 +6,28 @@ obj = Flask(__name__)
 def welcome():
     return "Welcome to Flask"
 
-@obj.route('/cal', methods = ['GET'])
+@obj.route('/cal', methods = ['GET', 'POST'])
 def math_operations():
-    operations = request.json["operations"]
-    number1 = request.json["number1"]
-    number2 = request.json["number2"]
-
-    if operations == "add":
-        result = int(number1) + int(number2)
-    elif operations == "multiply":
-        result = int(number1) * int(number2)
-    elif operations == "division":
-        result = int(number1) / int(number2)
+    if request.method == "GET":
+        return render_template('result.html')
+        # operations = request.form["operations"]
+        # number1 = request.form["number1"]
+        # number2 = request.form["number2"]
     else:
-        result = int(number1) - int(number2)
-    return f"The operations is {operations} and the result is {result}"
+        operations = request.form["operations"]
+        number1 = request.form["number1"]
+        number2 = request.form["number2"]
+
+        if operations == "add":
+            result = int(number1) + int(number2)
+        elif operations == "multiply":
+            result = int(number1) * int(number2)
+        elif operations == "division":
+            result = int(number1) / int(number2)
+        else:
+            result = int(number1) - int(number2)
+        return f"The operations is {operations} and the result is {result}"
+        #return redirect(url_for(result,str(result)))
 
 
 print(__name__)
